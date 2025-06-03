894da9225563d96da4e26e88644cef22775d4427
Ismael Loko <lokoismael30@gmail.com>
2025-06-03 03:36:50 -0400

    challenge: Button with children

---
+++ Reusability/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <main className="flex flex-col justify-center item-center w-2/3 mx-auto mt-10">
+      <div>
+        <Button>Buy now</Button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ Reusability/src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default function Button(props) {
+  return <button className="bg-blue-500">{props.children}</button>;
+}
