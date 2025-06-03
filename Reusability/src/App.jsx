dd5d954168f948c006d8b64335de2569bc74c899
Ismael Loko <lokoismael30@gmail.com>
2025-06-03 03:41:46 -0400

    using react-icons

--- Reusability/src/App.jsx
+++ Reusability/src/App.jsx
@@ -1,10 +1,14 @@
+import { FaBitcoin } from "react-icons/fa6";
+
 import Button from "./components/Button";
 
 function App() {
   return (
     <main className="flex flex-col justify-center item-center w-2/3 mx-auto mt-10">
       <div>
-        <Button>Buy now</Button>
+        <Button>
+          <FaBitcoin /> Buy now!
+        </Button>
       </div>
     </main>
   );
--- Reusability/src/components/Button.jsx
+++ Reusability/src/components/Button.jsx
@@ -1,3 +1,7 @@
 export default function Button(props) {
-  return <button className="bg-blue-500">{props.children}</button>;
+  return (
+    <button className="bg-blue-500 border-2 border-black px-4 py-2 rounded flex items-center gap-2 font-bold text-white text-lg">
+      {props.children}
+    </button>
+  );
 }
